6efe921eacdfc1c34bdb2d4ed7471b99ab70aa74
nikdmello <nik.dmello@gmail.com>
2019-08-09 14:02:02 +0400

    Initial Commit

---
+++ Listify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Listify
+//
+//  Created by Nikhil D'Mello on 8/9/19.
+//  Copyright © 2019 Nikhil D'Mello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
